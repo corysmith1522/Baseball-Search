8e57252325460160ae1d8e9ca56de8088f11cf1e
Cory Smith <corysmith@m-c02qp8v8g8wp.fios-router.home>
2020-09-21 14:59:34 -0400

    start js file

---
+++ app.js
@@ -0,0 +1,3 @@
+//Get player information: https://api.sportsdata.io/v3/mlb/scores/json/Player/${player-name}?key=23c95a605da34a6da64a5a2e44160c36
+//Get player statistics: http://api.sportradar.us/mlb/trial/v7/en/players/${player-name}/profile.json?api_key=rwdtx9v3uxsadnpwhaf6r6xd
+
