0d36eadaec7806e8ab3233eaa74230f027a741d5
Cory Smith <corysmith@m-c02qp8v8g8wp.fios-router.home>
2020-09-22 13:28:41 -0400

    js and html fixes

--- app.js
+++ app.js
@@ -2,24 +2,34 @@
 //Get player statistics: http://api.sportradar.us/mlb/trial/v7/en/players/${player-name}/profile.json?api_key=rwdtx9v3uxsadnpwhaf6r6xd
 
 let search = document.querySelector('#player-search')
-let button = document.querySelector('#search-button')
-button.addEventListener('click', async (event) => {
-  getName()
-  event.preventDefault();
-})
-async function getName() {
+
+async function getName(team) {
   try {
-    let response = await axios.get(`https://api.sportsdata.io/v3/mlb/scores/json/Players?key=23c95a605da34a6da64a5a2e44160c36`)
-    // console.log(response);
-    response.data.forEach(player => {
-      let fullName = `${player.FirstName} ${player.LastName} ${player.Position}`
-      // let input = "Chase Anderson"
-      if (search.value === fullName) {
-        console.log(player)
-      }
-    })
-    console.log(search.value) //the person's input
+    let response = await axios.get(`https://api.sportsdata.io/v3/mlb/scores/json/Players/${team}?key=23c95a605da34a6da64a5a2e44160c36`)
+    let active = response.data.filter(player => player.Status === "Active")
+    // console.log(active)
+    printData(active)
   } catch (error) {
     console.log(`Error: ${error}`)
   }
+}
+
+const form = document.querySelector('form')
+form.addEventListener('submit', inputValue)
+
+function inputValue(e) {
+  e.preventDefault()
+  const inputValue = document.querySelector('select').value
+  // console.log(inputValue)
+  getName(inputValue)
+}
+
+function printData(active) {
+  active.forEach(player => {
+    console.log(`${player.FirstName} ${player.LastName} `)
+    // console.log(player)
+    let p = document.createElement('p')
+    p.textContent = `${player.Jersey} | ${player.FirstName} ${player.LastName} | ${player.Position} | ${player.BirthCity}, ${player.BirthState || player.BirthCountry} | ${player.BatHand}, ${player.ThrowHand}`
+    document.querySelector('#player-stats').append(p)
+  })
 }
