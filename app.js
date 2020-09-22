0cf483029366eb6887181f341c10fc221bb35da5
Cory Smith <corysmith@m-c02qp8v8g8wp.fios-router.home>
2020-09-22 16:02:24 -0400

    add remove function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 let search = document.querySelector('#player-search')
 
 async function getName(team) {
+  removeData()
   try {
     let response = await axios.get(`https://api.sportsdata.io/v3/mlb/scores/json/Players/${team}?key=23c95a605da34a6da64a5a2e44160c36`)
     let active = response.data.filter(player => player.Status === "Active")
@@ -18,6 +19,7 @@ const form = document.querySelector('form')
 form.addEventListener('submit', inputValue)
 
 function inputValue(e) {
+  // removeData()
   e.preventDefault()
   const inputValue = document.querySelector('select').value
   // console.log(inputValue)
@@ -25,9 +27,13 @@ function inputValue(e) {
 }
 
 function printData(active) {
+  // removeData()
   active.forEach(player => {
     console.log(`${player.FirstName} ${player.LastName} `)
     // console.log(player)
+    let div = document.createElement('div')
+    div.setAttribute("id", "player-stats")
+    document.querySelector('.player-container').append(div)
     let p = document.createElement('p')
     p.textContent = `#${player.Jersey} | ${player.FirstName} ${player.LastName} | Position: ${player.Position} | Birthplace: ${player.BirthCity}, ${player.BirthState || player.BirthCountry} | Bats: ${player.BatHand}, Throws: ${player.ThrowHand} | College: ${player.College || ' '}`
     document.querySelector('#player-stats').append(p)
@@ -35,4 +41,11 @@ function printData(active) {
     img.setAttribute('src', player.PhotoUrl)
     document.querySelector('#player-stats').append(img)
   })
+}
+
+function removeData() {
+  const removeElement = document.querySelector('.player-container')
+  while (removeElement.lastChild) {
+    removeElement.removeChild(removeElement.lastChild)
+  }
 }
