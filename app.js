3bdfce6846db08e728160289b44b470eaf8e2376
Cory Smith <corysmith@m-c02qp8v8g8wp.fios-router.home>
2020-09-21 23:20:33 -0400

    success return name via search

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 //Get player information: https://api.sportsdata.io/v3/mlb/scores/json/Player/${player-name}?key=23c95a605da34a6da64a5a2e44160c36
 //Get player statistics: http://api.sportradar.us/mlb/trial/v7/en/players/${player-name}/profile.json?api_key=rwdtx9v3uxsadnpwhaf6r6xd
+
 let search = document.querySelector('#player-search')
+let button = document.querySelector('#search-button')
+button.addEventListener('click', async (event) => {
+  getOptions();
+  event.preventDefault();
+});
 async function getOptions() {
-  const url = `https://api.sportsdata.io/v3/mlb/scores/json/Players?key=23c95a605da34a6da64a5a2e44160c36`
   try {
-    const response = await axios.get(url)
-    const data = response.data
-    console.log(search.value)
-    console.log(filterName(data, search.value))
-    // for (let i = 0; i < data.length; i++) {
-    //   let name = `${response.data[i].FirstName} ${response.data[i].LastName}`
-    //   console.log(name)
-    // }
+    // const url = ``
+    const response = await axios.get(`https://api.sportsdata.io/v3/mlb/scores/json/Players?key=23c95a605da34a6da64a5a2e44160c36`)
+    // console.log(response);
+    response.data.forEach(player => {
+      let fullName = `${player.FirstName} ${player.LastName}`
+      // let input = "Chase Anderson"
+      if (search.value === fullName) {
+        console.log(player)
+      }
+    })
+    console.log(search.value) //the person's input
   } catch (error) {
     console.log(`Error: ${error}`)
   }
 }
-
-getOptions()
-
-function filterName(array, value) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      return true
-    }
-  }
-  return false
-}
-console.log(filterName([1, 2, 3], 7))
